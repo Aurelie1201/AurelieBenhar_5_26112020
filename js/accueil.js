907f0e32218fc3b69a5118c0cf580ac1afdd0dad
Aurelie Benhar <aurelie.120183@hotmail.fr>
2021-04-07 14:00:57 +0200

    Navigation responsive page accueil, nom et prix des ours

--- js/accueil.js
+++ js/accueil.js
@@ -2,21 +2,23 @@ promise
     .then(function(response){
 
         let url;
-
+            //////////////////création d'une ligne tous les 2 produits////////////////////
         for(let i = 0; i < response.length; i++){
             if ( i%2 == 0){
             ajoutLigne(document.getElementById('container')); 
             }
         }
-
+            //////////////////création de 2 colonnes pour chaque ligne////////////////////
         for(let i = 0; i < document.getElementsByClassName('row').length; i++){
             ajoutColonne(document.getElementsByClassName('row')[i]);
             ajoutColonne(document.getElementsByClassName('row')[i]);
         }
-
+            //////////////////création d'une fiche produit pour chaque produit////////////////////
         for(let i = 0; i < response.length; i++){
             url = response[i].imageUrl;
-            document.getElementsByClassName('col-6')[i].innerHTML = '<img alt="image ourson" src="' + url +'"/>';
+            nom = response[i].name;
+            prix = response[i].price/100;
+            document.getElementsByClassName('col-md-6')[i].innerHTML = creationCarteProduit(url, nom, prix);
         }
     })
     
@@ -32,6 +34,10 @@ const ajoutLigne = (element) => {
 
 const ajoutColonne = (element) => {
     const col = document.createElement("div");
-    col.classList.add("col-6");
+    col.classList.add("col-md-6");
     element.appendChild(col);
+}
+
+const creationCarteProduit = (url, nom, prix) => {
+    return '<figure><img alt="image ourson" src="'+ url +'"/><figcaption>'+ nom +' '+ prix +'€</figcaption></figure>';
 }
